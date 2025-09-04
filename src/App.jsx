8ed0cfd995f98635a7e83385da15b19608c7b359
Kaue <ksoaresmadarazzo@gmail.com>
2025-09-04 14:24:29 -0300

    corringindo erros

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react"
 import Calculadora from "./components/Calculadora"
 import Header from "./components/Header"
 
+
 function App() {
 
   // hook para atuaizar numero na tela
