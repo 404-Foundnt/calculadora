38c969e400472e8d2fadf1a2f98836f18bca50d4
Rafael <seijiarakaki12@gmail.com>
2025-09-04 17:30:57 -0300

    estilizando botoes

--- src/components/Calculadora.jsx
+++ src/components/Calculadora.jsx
@@ -4,29 +4,33 @@ import "../css/Calculadora.css"
 export default function Calculadora() {
     return (
         <section>
-            <div>
+            <div className="numeros">
+                <div>
                 <button>7</button>
                 <button>8</button>
                 <button>9</button>
                 <button>/</button>
-            </div>
-            <div>
+                </div>
+                <div>
                 <button>4</button>
                 <button>5</button>
                 <button>6</button>
                 <button>*</button>
-            </div>
-            <div>
+                </div>
+                <div>
                 <button>1</button>
                 <button>2</button>
                 <button>3</button>
                 <button>-</button>
-            </div>
-            <div>
+                </div>
+                <div>
+    
                 <button>0</button>
                 <button> ,</button>
                 <button>=</button>
                 <button>+</button>
+                </div>
+  
             </div>
         </section>
     )
