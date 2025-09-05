aa46a927c53e1ae288af2881ff2ddebcadc31816
Rafael <rafealnakaya@gmail.com>
2025-09-05 20:13:59 -0300

    Adicionando o css e logo no Header

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,11 @@ import { useState } from "react"
 import Calculadora from "./components/Calculadora"
 import Header from "./components/Header"
 
-
 function App() {
 
   // hook para atuaizar numero na tela
   const [resultado,setresultado] = useState(0)
  
-
-
 
   return (
     <>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import "..//css/Header.css"
 import "..//css/Calculadora.css"
+import logo from '../assets/404.png'
 
 function Header() {
-    return (
-        <div className="header">
-            <h1>Calculadora</h1>
-        </div>
-    )
-
+  return (
+    <div className="header">
+      <img src={logo} alt="logo" className="logo" />
+      <h1>Calculadora</h1>
+    </div>
+  );
 }
-export default Header
+
+export default Header;
